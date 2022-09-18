ffa09215d95180b154b9ba72f8ede198eff0f466
David Reed <reedswork@proton.me>
2022-09-18 01:08:22 -0400

    fix isFailedToFetch

--- src/isAbortError.ts
+++ src/isAbortError.ts
@@ -8,7 +8,7 @@ export const isError = (err: unknown): err is Error =>
 	typeof err === 'object' && err !== null && err instanceof Error;
 
 export const isFailedToFetch = (err: unknown): err is Error =>
-	isError(err) && err.message === '';
+	isError(err) && err.message === 'Failed to fetch';
 
 const isAbortError = (err: unknown): err is Error =>
 	isError(err) && messages.includes(err.message);
