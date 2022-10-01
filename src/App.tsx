e643a15b0060d9bc7d22cf99b89ab257ebc6ebbb
David Reed <reedswork@proton.me>
2022-09-30 20:20:10 -0400

    trade common.scss imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './styles/root.scss';
 import './styles/fonts/Lato.scss';
 import './styles/fonts/MontserratAlternates.scss';
 import './styles/theme.scss';
-import './styles/common.scss';
 import type { CompatLayoutRef } from './CompatLayout';
 import CompatLayout from './CompatLayout';
 import type { LayoutRef } from './Layout';
