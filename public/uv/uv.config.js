63d28a8718739232ec07c5c38fbc9703a3420f29
David Reed <reedswork@proton.me>
2023-02-11 13:08:57 -0500

    drop poor uv types, specify client script

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,27 +1,5 @@
-/**
- * @typedef {(encoded: string) => string} UVEncode
- */
-
-/**
- * @typedef {(encoded: string) => string} UVDecode
- */
-
-/**
- * @typedef {{ encode: UVEncode, decode: UVDecode }} UVCodec
- */
-
-/**
- * @typedef {{ codec: Record<string, { codec: Record<string, UVCodec> } }} Ultraviolet
- */
-
-/**
- * @typedef {{ bare: string; prefix: string; handler: string; bundle: string; config: string; sw: string; encodeUrl: UVEncode; decodeUrl: UVDecode }} UVConfig
- */
-
+/*global Ultraviolet*/
 {
-	/** @type {Ultraviolet} */
-	const Ultraviolet = globalThis.Ultraviolet;
-
 	/**
 	 *
 	 * @param {string} env
@@ -35,9 +13,6 @@
 			.replace('%{location.protocol}', protocol);
 	};
 
-	/**
-	 * @type {UVConfig}
-	 */
 	const config = {
 		prefix: `${process.env.PUBLIC_PATH}/${process.env.REACT_APP_UV_DIR}/service/`,
 		bare: process.env.REACT_APP_BARE_APIS.split(',').map(format),
@@ -46,6 +21,7 @@
 		handler: `${process.env.PUBLIC_PATH}/${process.env.REACT_APP_UV_DIR}/uv.handler.js`,
 		bundle: `${process.env.PUBLIC_PATH}/${process.env.REACT_APP_UV_DIR}/uv.bundle.js`,
 		config: `${process.env.PUBLIC_PATH}/${process.env.REACT_APP_UV_DIR}/uv.config.js`,
+		client: `${process.env.PUBLIC_PATH}/${process.env.REACT_APP_UV_DIR}/uv.client.js`,
 		sw: `${process.env.PUBLIC_PATH}/${process.env.REACT_APP_UV_DIR}/uv.sw.js`,
 	};
 
