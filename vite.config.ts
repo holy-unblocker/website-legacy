4d690a2d7b3ef7111b113a13359c3c94968cc5b8
David Reed <reedswork@proton.me>
2023-06-12 13:21:51 -0400

    required env vars

--- vite.config.ts
+++ vite.config.ts
@@ -5,75 +5,94 @@ import { uvPath } from '@titaniumnetwork-dev/ultraviolet';
 import react from '@vitejs/plugin-react-swc';
 import { createRequire } from 'node:module';
 import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
+import type { ConfigEnv } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import svgr from 'vite-plugin-svgr';
 
-process.env.VITE_PUBLIC_PATH ??= '';
-
-const require = createRequire(import.meta.url);
-
-const routes = getRoutes(
-	process.env.VITE_ROUTER as RouteType,
-	process.env.VITE_PUBLIC_PATH
-);
-
 // https://vitejs.dev/config/
-export default defineConfig({
-	build: {
-		sourcemap: true,
-		rollupOptions: {
-			plugins: [
-				{
-					name: 'copy index.html to all the hot routes',
-					async generateBundle(options, bundle) {
-						const index = bundle['index.html'];
+export default function viteConfig({ mode }: ConfigEnv) {
+	Object.assign(process.env, loadEnv(mode, process.cwd()));
+	process.env.VITE_PUBLIC_PATH ??= '';
 
-						for (const file of routes)
-							bundle[file.file] = {
-								...index,
-								fileName: file.file,
-							};
+	const require = createRequire(import.meta.url);
+
+	for (const required of [
+		'VITE_ROUTER',
+		'VITE_SUPPORT_EMAIL',
+		'VITE_HAT_BADGE',
+		'VITE_DEFAULT_PROXY',
+		'VITE_TN_DISCORD_URL',
+		'VITE_HU_DISCORD_URL',
+		'VITE_BARE_APIS',
+		'VITE_RH_API',
+		'VITE_DB_API',
+		'VITE_THEATRE_CDN',
+	])
+		if (!(required in process.env))
+			throw new TypeError(`Missing environment variable: ${required}`);
+
+	const routes = getRoutes(
+		process.env.VITE_ROUTER as RouteType,
+		process.env.VITE_PUBLIC_PATH
+	);
+
+	return defineConfig({
+		build: {
+			sourcemap: true,
+			rollupOptions: {
+				plugins: [
+					{
+						name: 'copy index.html to all the hot routes',
+						async generateBundle(options, bundle) {
+							const index = bundle['index.html'];
+
+							for (const file of routes)
+								bundle[file.file] = {
+									...index,
+									fileName: file.file,
+								};
+						},
 					},
-				},
-			],
-		},
-	},
-	plugins: [
-		{
-			name: 'serve index.html for all the hot routes',
-			configureServer(server) {
-				server.middlewares.use((req, _res, next) => {
-					let url = req.url;
-					const search = url.indexOf('?');
-					if (search !== -1) url = url.slice(0, search);
-					if (routes.some((route) => route.path === url))
-						req.url = '/index.html';
-					next();
-				});
+				],
 			},
 		},
-		react(),
-		svgr(),
-		viteStaticCopy({
-			targets: [
-				{
-					src: uvPath,
-					dest: '',
-					rename: 'uv',
-					overwrite: false,
+		plugins: [
+			{
+				name: 'serve index.html for all the hot routes',
+				configureServer(server) {
+					server.middlewares.use((req, _res, next) => {
+						let url = req.url;
+						const search = url.indexOf('?');
+						if (search !== -1) url = url.slice(0, search);
+						if (routes.some((route) => route.path === url))
+							req.url = '/index.html';
+						next();
+					});
 				},
-				{
-					src: stompPath,
-					dest: '',
-					rename: 'stomp',
-				},
-				{
-					src: resolve(require.resolve('@ruffle-rs/ruffle'), '..'),
-					dest: '',
-					rename: 'ruffle',
-				},
-			],
-		}),
-	],
-});
+			},
+			react(),
+			svgr(),
+			viteStaticCopy({
+				targets: [
+					{
+						src: uvPath,
+						dest: '',
+						rename: 'uv',
+						overwrite: false,
+					},
+					{
+						src: stompPath,
+						dest: '',
+						rename: 'stomp',
+					},
+					{
+						src: resolve(require.resolve('@ruffle-rs/ruffle'), '..'),
+						dest: '',
+						rename: 'ruffle',
+					},
+				],
+			}),
+		],
+	});
+}
